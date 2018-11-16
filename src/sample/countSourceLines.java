53c5b98305f3ebf8d3c91ed483ac5bcd0cb326d8
Jodilyn Peralta <jodilynperalta@gmail.com>
2018-11-15 21:25:20 -0800

    Function to get sourcelines

--- src/sample/countSourceLines.java
+++ src/sample/countSourceLines.java
@@ -1,5 +1,25 @@
 package sample;
 
+import java.io.*;
+
 public class countSourceLines
 {
+    int getSourceLines(File name) throws IOException
+    {
+        Reader readf = new FileReader(name);
+        StreamTokenizer st = new StreamTokenizer(readf);
+        st.slashSlashComments(false);
+        st.slashStarComments(false);
+        st.commentChar('/');
+        st.eolIsSignificant(true);
+        int lines = 0;
+        while(st.nextToken() != st.TT_EOF)
+        {
+            if(st.nextToken() == st.TT_EOL)
+            {
+                lines++;
+            }
+        }
+        return lines;
+    }
 }
