8960022d311b4841ba25157620bf1b2637c54aef
Jodilyn Peralta <jodilynperalta@gmail.com>
2018-11-25 23:46:15 -0800

    String array with only word operators so far

--- src/sample/Halstead.java
+++ src/sample/Halstead.java
@@ -2,6 +2,14 @@ package sample;
 
 public class Halstead
 {
+    /*special case words for java operators*/
+    String[] javaOperators{"break","case","continue","default","do","if","else","enum","for","goto","if","new",
+        "return","asm","operator","private","protected","public","sizeof","struct","switch","union","while",
+        "this","namespace","using","try","catch","throw","throws","finally","strictfp","instanceof","interface",
+        "extends","implements","abstract","concrete","const_cast","static_cast","dynamic_cast","reinterpret_cast",
+        "typeid","template","explicit","true","false","typename","const","friend","volatile","transient","final",
+        "auto","extern","register","static","typedef","virtual,","mutable","inline"};
+    String[] CPlusOperators;
     public int getDistinctOperator()
     {
 
@@ -32,6 +40,6 @@ public class Halstead
     }
     public int getEffort()
     {
-        
+
     }
 }
