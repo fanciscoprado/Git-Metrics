3e60aa1cc1721d6d0cd21238b85a8246034597d6
Jodilyn Peralta <jodilynperalta@gmail.com>
2018-11-25 23:05:33 -0800

    Layout for Halstead

---
+++ src/sample/Halstead.java
@@ -0,0 +1,37 @@
+package sample;
+
+public class Halstead
+{
+    public int getDistinctOperator()
+    {
+
+    }
+    public int getTotalOperator()
+    {
+
+    }
+    public int getVocab()
+    {
+
+    }
+    public int getPLength()
+    {
+
+    }
+    public int getCPLength()
+    {
+
+    }
+    public int getVolume()
+    {
+
+    }
+    public int getDifficulty()
+    {
+
+    }
+    public int getEffort()
+    {
+        
+    }
+}
