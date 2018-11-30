864b06af0eeb89e2dd3d76befa41398866615f0b
Jodilyn Peralta <jodilynperalta@gmail.com>
2018-11-29 21:47:34 -0800

    Added list of valid C++ operators

--- src/sample/HalsteadOps.java
+++ src/sample/HalsteadOps.java
@@ -11,7 +11,13 @@ public class HalsteadOps
             "const_cast","static_cast","dynamic_cast","reinterpret_cast", "typeid","template","explicit","true","false",
             "typename","const","friend","volatile","transient","final", "auto","extern","register","static","typedef",
             "virtual,", "mutable","inline","add","remove"};
-    String[] CPlusOperators;
+    String[] CPlusOperators = {"!","!=","%","%=","&","&&","||","&=","(",")","{","}","[","]","*","*=","+","++","+=",",",
+            "-","--","-=","->",".","...","/","/=",":","::","<","<<","<<=","<=","=","==",">",">=",">>",">>=","?","^","^=",
+            "|","|=","~",";","=&","ô","ô","æ","æ","#","##","~","break","case","continue","default","do","if","else","enum",
+            "for","goto","if","new","return","asm","operator","private","protected","public","sizeof","struct","switch",
+            "union","while","this","namespace","using,","try","catch","throw","abstract","concrete","const_cast","static_cast",
+            "dynamic_cast","reinterpret_cast","typeid","template","explicit","true","false","typename","const","friend","volatile",
+            "final","auto","extern","register","static","typedef","virtual","mutable","inline"};
     public int getDistinctOperator()
     {
         return 0;
