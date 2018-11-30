635ea901f72bce11f9ecb14741d239c9b38b65db
Jodilyn Peralta <jodilynperalta@gmail.com>
2018-11-29 21:27:25 -0800

    put getting operators and operands in its own class

--- src/sample/Halstead.java
+++ src/sample/Halstead.java
@@ -2,22 +2,7 @@ package sample;
 
 public class Halstead
 {
-    /*special case words for java operators*/
-    String[] javaOperators= {"break","case","continue","default","do","if","else","enum","for","goto","if","new",
-        "return","asm","operator","private","protected","public","sizeof","struct","switch","union","while",
-        "this","namespace","using","try","catch","throw","throws","finally","strictfp","instanceof","interface",
-        "extends","implements","abstract","concrete","const_cast","static_cast","dynamic_cast","reinterpret_cast",
-        "typeid","template","explicit","true","false","typename","const","friend","volatile","transient","final",
-        "auto","extern","register","static","typedef","virtual,","mutable","inline","add","remove"};
-    String[] CPlusOperators;
-    public int getDistinctOperator()
-    {
-        return 0;
-    }
-    public int getTotalOperator()
-    {
-        return 0;
-    }
+
     public int getVocab(int operator, int operand)
     {
         int temp = operator + operand;
