a594ac1f2fd6e7e71b3344f933e4da2a47da4883
MichaelN <nguyen.mikey@yahoo.com>
2018-12-05 05:14:57 -0800

    Variables declared in correct method.

--- src/sample/countDepthOfInheritance.java
+++ src/sample/countDepthOfInheritance.java
@@ -6,9 +6,11 @@ import java.io.*;
 import java.util.*;
 
 public class countDepthOfInheritance {
-    int inheritance = 1;
-    String fileName;
-    String[]keywords = {"class"};
-    String words = "";        //temp variable to hold value of token
-    String line = null;
+    public int checkForInheritance (){
+        int inheritance = 1;
+        String fileName;
+        String[]keywords = {"class"};
+        String words = "";        //temp variable to hold value of token
+        String line = null;
+    }
 }
