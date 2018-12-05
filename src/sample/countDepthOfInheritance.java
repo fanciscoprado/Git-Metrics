69e626f2b6492a8e4f4a40706679262619f16400
MichaelN <nguyen.mikey@yahoo.com>
2018-12-05 05:11:31 -0800

    Variables initialized.

--- src/sample/countDepthOfInheritance.java
+++ src/sample/countDepthOfInheritance.java
@@ -1,9 +1,14 @@
 package sample;
 
-//experimental class, don't know if this will work properly
+//experimental class, don't know if this will work properly. DoI will follow CC algorithm
 
 import java.io.*;
 import java.util.*;
 
 public class countDepthOfInheritance {
+    int inheritance = 1;
+    String fileName;
+    String[]keywords = {"class"};
+    String words = "";        //temp variable to hold value of token
+    String line = null;
 }
