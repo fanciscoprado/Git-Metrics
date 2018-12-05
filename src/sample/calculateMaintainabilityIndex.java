0c406c3f9a8eb378727b9b866431c0c68bab7bff
MichaelN <nguyen.mikey@yahoo.com>
2018-12-05 05:33:16 -0800

    Reference for Maintainability Index formula added.

--- src/sample/calculateMaintainabilityIndex.java
+++ src/sample/calculateMaintainabilityIndex.java
@@ -1,4 +1,5 @@
 package sample;
 
+//Formula(s) for calculating maintainability index found here: http://www.projectcodemeter.com/cost_estimation/help/GL_maintainability.htm
 public class calculateMaintainabilityIndex {
 }
