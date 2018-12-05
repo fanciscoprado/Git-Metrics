8835cf5e930d334f97bdc565d4f424d223920521
MichaelN <nguyen.mikey@yahoo.com>
2018-12-05 05:37:39 -0800

    Comments about Maintainability index and required values added.

--- src/sample/calculateMaintainabilityIndex.java
+++ src/sample/calculateMaintainabilityIndex.java
@@ -1,5 +1,13 @@
 package sample;
 
+//NEEDS FOLLOWING TO CALCULATE: halstead volume, cyclomatic complexity, count of source lines of code.
+
 //Formula(s) for calculating maintainability index found here: http://www.projectcodemeter.com/cost_estimation/help/GL_maintainability.htm
+
 public class calculateMaintainabilityIndex {
+    //Information that may or may not be implemented in output in the future.
+    //Value 1-100, higher = better
+    //20-100 = good
+    //10-19 = moderate
+    //0-9 = low
 }
