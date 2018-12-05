700a7ecfbd439a31ec0c8084688c0755bdcca160
MichaelN <nguyen.mikey@yahoo.com>
2018-12-05 05:19:43 -0800

    Algorithm implemented and changed to fit Inheritance class.

--- src/sample/countDepthOfInheritance.java
+++ src/sample/countDepthOfInheritance.java
@@ -1,6 +1,6 @@
 package sample;
 
-//experimental class, don't know if this will work properly. DoI will follow CC algorithm
+//experimental class, don't know if this will work properly. DoI will follow CC algorithm.
 
 import java.io.*;
 import java.util.*;
@@ -18,6 +18,22 @@ public class countDepthOfInheritance {
             FileReader fr = new FileReader(fileName);
             BufferedReader br = new BufferedReader(fr);
             line = br.readLine();
+            while (line != null)
+            {
+                StringTokenizer stTokenizer = new StringTokenizer(line);
+                while (stTokenizer.hasMoreTokens())
+                {
+                    words = stTokenizer.nextToken();
+                    for(int i=0; i<keywords.length; i++)
+                    {
+                        if(keywords[i].equals(words))        //compares token (words) to see if it is keyword
+                        {
+                            inheritance++;
+                        }
+                    }
+                }
+                line = br.readLine();
+            }
         }
         catch (IOException e)
         {
