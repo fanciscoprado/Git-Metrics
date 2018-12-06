bfa98418893a2b9b919c0b04aaec465f28b5e629
ckiyomura <ckiyomura@columba.xyz>
2018-12-05 19:46:54 -0800

    built a simple run method. may need to refactor both name and code

--- src/sample/halstead/definitionList.java
+++ src/sample/halstead/definitionList.java
@@ -1,8 +1,21 @@
 package sample.halstead;
 
-public class definitionList {
-    public string[] run()throws Exception
-    {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
+public class definitionList {
+    public String[] run()throws Exception
+    {
+        FileReader reader = new FileReader("test.txt");
+        BufferedReader bReader = new BufferedReader(reader);
+        List<String> lines = new ArrayList<String>();
+        String line = null;
+        while ((line = bReader.readLine()) != null){
+            lines.add(line);
+        }
+        bReader.close();
+        return lines.toArray(new String[lines.size()]);
     }
 }
