c8d08ec21806878d5e4c8128a44d173cf0e24c7b
ckiyomura <ckiyomura@columba.xyz>
2018-12-06 01:30:28 -0800

    Started work on writing the compute method. Refactored buildCodeList into its own java file

---
+++ src/sample/halstead/codeList.java
@@ -0,0 +1,49 @@
+package sample.halstead;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.StreamTokenizer;
+import java.util.ArrayList;
+
+public class codeList {
+
+    final char[] HALF_OPERATOR = {'=', '+', '-', '*', '/', '%','!', '>', '<', '&', '|', '?', '~', '^', ':', '@', '_'};
+
+    //collects all relevant elements for Halstead metrics
+    ArrayList<String> codeList = new ArrayList<>();
+
+    //Creates an indexed list of all the elements from stream tokenizer
+    public void setCodeList(File currentFile)throws Exception
+    {
+        BufferedReader readFile = new BufferedReader(new FileReader(currentFile));
+        StreamTokenizer fileStream = new StreamTokenizer(readFile);
+        char tempChar;
+        StringBuilder tempString = new StringBuilder();
+        fileStream.slashStarComments(true);
+        while(fileStream.nextToken() != fileStream.TT_EOF){
+            if(fileStream.sval==null){
+                tempChar = (char)fileStream.ttype;
+                for (int i = 0; i < HALF_OPERATOR.length; i++) {
+                    if(HALF_OPERATOR[i] == tempChar) {
+                        tempString.append(tempChar);
+                        break;
+                    }
+                    if((i+1)==HALF_OPERATOR.length){
+                        if(tempString.length() > 0){
+                            codeList.add(tempString.toString());
+                            tempString = new StringBuilder();
+                        }
+                    }
+                }
+            } else {
+                if(tempString.length() > 0){
+                    codeList.add(tempString.toString());
+                    tempString = new StringBuilder();
+                }
+                codeList.add(fileStream.sval);
+            }
+        }
+        readFile.close();
+    }
+}
--- src/sample/halstead/computeHalstead.java
+++ src/sample/halstead/computeHalstead.java
@@ -5,17 +5,19 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.StreamTokenizer;
 import java.util.ArrayList;
+import java.util.List;
 
 public class computeHalstead {
 
-    final char[] HALF_OPERATOR = {'=', '+', '-', '*', '/', '%','!', '>', '<', '&', '|', '?', '~', '^', ':', '@', '_'};
-
-    //collects all relevant elements for Halstead metrics
-    ArrayList<String> codeList = new ArrayList<>();
+    List<String> Operators = new ArrayList<>();
+    List<String> Operands = new ArrayList<>();
 
     public void compute(File currentFile)throws Exception
     {
-        buildCodeList(currentFile);
+        codeList codes = new codeList();
+        vocabularyList vocab = new vocabularyList();
+        codes.setCodeList(currentFile);
+        vocab.setVocabList("HalLib");
         //needs to do a lot more than this
         //it needs a vocab list from vocab constructor
         //it needs to compare elements from the codeList to the vocab list and delegate operator or operand status
@@ -24,37 +26,10 @@ public class computeHalstead {
         //calculating secondary and tertiary stats will likely be this classes job as well
     }
 
-    //Creates an indexed list of all the elements from stream tokenizer
-    private void buildCodeList(File currentFile)throws Exception
-    {
-        BufferedReader readFile = new BufferedReader(new FileReader(currentFile));
-        StreamTokenizer fileStream = new StreamTokenizer(readFile);
-        char tempChar;
-        StringBuilder tempString = new StringBuilder();
-        fileStream.slashStarComments(true);
-        while(fileStream.nextToken() != fileStream.TT_EOF){
-            if(fileStream.sval==null){
-                tempChar = (char)fileStream.ttype;
-                for (int i = 0; i < HALF_OPERATOR.length; i++) {
-                    if(HALF_OPERATOR[i] == tempChar) {
-                        tempString.append(tempChar);
-                        break;
-                    }
-                    if((i+1)==HALF_OPERATOR.length){
-                        if(tempString.length() > 0){
-                            codeList.add(tempString.toString());
-                            tempString = new StringBuilder();
-                        }
-                    }
-                }
-            } else {
-                if(tempString.length() > 0){
-                    codeList.add(tempString.toString());
-                    tempString = new StringBuilder();
-                }
-                codeList.add(fileStream.sval);
-            }
-        }
-        readFile.close();
+    //Another test, another main
+    public static void main(String[] args)throws Exception{
+        computeHalstead test = new computeHalstead();
+        File testFile = new File("src/sample/wc.java");
+        test.compute(testFile);
     }
 }
