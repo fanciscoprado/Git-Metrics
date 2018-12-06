006d3ee69b5b6c004e84a9298e3669d76ca0ba91
ckiyomura <ckiyomura@columba.xyz>
2018-12-06 01:00:31 -0800

    VocabConstructor is mostly complete. It is now named vocabularyList

--- src/sample/halstead/vocabularyList.java
+++ src/sample/halstead/vocabularyList.java
@@ -1,7 +1,5 @@
 package sample.halstead;
 
-import org.eclipse.jgit.util.FileUtils;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -9,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class VocabConstructor
+public class vocabularyList
 {
     //collects operators and their positional information
     private ArrayList<halsteadOperator> vocabList = new ArrayList<>();
@@ -23,7 +21,6 @@ public class VocabConstructor
             //halsteadOperator has three elements. The String array being read from stores them in this order:
             //name, if its a prefix and if its a postfix
             s = operatorList[index];
-            System.out.print(s);
             if(index % 3 == 0)
             {
                 temp = new halsteadOperator();
@@ -46,6 +43,7 @@ public class VocabConstructor
         System.out.println();
     }
 
+    //collects elements of a txt file into an array
     private String[] readFile(File file)throws Exception
     {
         FileReader reader = new FileReader(file);
@@ -60,17 +58,20 @@ public class VocabConstructor
         return lines.toArray(new String[lines.size()]);
     }
 
-    //made a main for testing, will delete later
-    public void buildDefinitions()throws Exception{
-        File folder = new File("HalLib");
+    public void setVocabList(String path)throws Exception
+    {
+        File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
-
-        for (int i = 0; i < listOfFiles.length; i++) {
-            File file = listOfFiles[i];
-            if (file.isFile() && file.getName().endsWith(".txt")) {
-                String[] arr = this.readFile(file);
-                addVocabElement(arr);
+        try {
+            for (int i = 0; i < listOfFiles.length; i++) {
+                File file = listOfFiles[i];
+                if (file.isFile() && file.getName().endsWith(".txt")) {
+                    String[] arr = this.readFile(file);
+                    addVocabElement(arr);
+                }
             }
+        } catch (NullPointerException e){
+            //catches null but doesn't throw anything yet
         }
     }
 
@@ -79,12 +80,12 @@ public class VocabConstructor
     }
 
     //doesn't need a main - but it is good for testing
-    public static void main(String[] args)throws Exception
+    /*public static void main(String[] args)throws Exception
     {
-        VocabConstructor test = new VocabConstructor();
-        test.buildDefinitions();
+        vocabularyList test = new vocabularyList();
+        test.buildDefinitions("HalLib");
         for(int i = 0; i < test.vocabList.size(); i++){
             System.out.println(test.vocabList.get(i));
         }
-    }
+    }*/
 }
