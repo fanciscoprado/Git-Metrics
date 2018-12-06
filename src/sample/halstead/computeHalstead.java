b7e8a7a022ea83b2dbe88164efcf00f30b36f265
ckiyomura <ckiyomura@columba.xyz>
2018-12-06 01:42:44 -0800

    Began a method for categorizing code and is called by the compute method.

--- src/sample/halstead/computeHalstead.java
+++ src/sample/halstead/computeHalstead.java
@@ -18,12 +18,19 @@ public class computeHalstead {
         vocabularyList vocab = new vocabularyList();
         codes.setCodeList(currentFile);
         vocab.setVocabList("HalLib");
+        categorizeElements(codes.getCodeList(), vocab.getVocabList());
+        //COMPARE ELEMENTS CALL
         //needs to do a lot more than this
         //it needs a vocab list from vocab constructor
         //it needs to compare elements from the codeList to the vocab list and delegate operator or operand status
         //      to individual elements -- or call another method to that.
         //this class as a whole needs to be able to calculate n1, n2, N1 and N2
         //calculating secondary and tertiary stats will likely be this classes job as well
+    }
+
+    private void categorizeElements(ArrayList<String> uncategorizedCode, ArrayList<halsteadOperator> definitions)
+    {
+
     }
 
     //Another test, another main
