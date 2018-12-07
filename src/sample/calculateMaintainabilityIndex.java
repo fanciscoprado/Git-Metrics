e97fa30ad3ca2c7188f5955ad92ced1be0352605
MichaelN <nguyen.mikey@yahoo.com>
2018-12-07 04:53:40 -0800

    Added raw formulas, still needs implementation

--- src/sample/calculateMaintainabilityIndex.java
+++ src/sample/calculateMaintainabilityIndex.java
@@ -2,6 +2,8 @@ package sample;
 package java.lang.Math;
 
 //NEEDS FOLLOWING TO CALCULATE: halstead volume, cyclomatic complexity, count of source lines of code.
+// Original formula: MI = 171 - 5.2 * ln(V) - 0.23 * (G) - 16.2 * ln(LOC)
+// Microsoft Visual Studio formula: MI = MAX(0,(171 - 5.2 * ln(Halstead Volume) - 0.23 * (Cyclomatic Complexity) - 16.2 * ln(Lines of Code))*100 / 171)
 
 //Formula(s) for calculating maintainability index found here: http://www.projectcodemeter.com/cost_estimation/help/GL_maintainability.htm
 
