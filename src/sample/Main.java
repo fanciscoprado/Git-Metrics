f5046fd2cdbc2f994c1b60f4bbed03b8714640e6
Chris Vejsada <chrisjvejsada@gmail.com>
2018-12-07 08:49:24 -0800

    Temp folder removal added in main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,11 +1,14 @@
 package sample;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
@@ -21,6 +24,9 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        File file = new File("temp/");
+        if(file.exists())
+            new GitController().close();
         launch(args);
     }
 }
