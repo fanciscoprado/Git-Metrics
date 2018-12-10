0b3b460b25df7f75bc16ba0021c8aa78bc6fa4d2
ckiyomura <ckiyomura@columba.xyz>
2018-12-09 18:22:56 -0800

    Resized columns to half their size

--- src/sample/DisplayMetricsData.java
+++ src/sample/DisplayMetricsData.java
@@ -14,60 +14,61 @@ public class DisplayMetricsData {
         table.setItems(list);
 
         TableColumn<DataCollected, String> nameColum = new TableColumn<>("File Name");
-        nameColum.setMinWidth(200);
+        nameColum.setMinWidth(100);
         nameColum.setCellValueFactory(new PropertyValueFactory<>("filename"));
 
         TableColumn<DataCollected, String> wordColum = new TableColumn<>("Word Count");
-        wordColum.setMinWidth(200);
+        wordColum.setMinWidth(100);
         wordColum.setCellValueFactory(new PropertyValueFactory<>("wcount"));
 
         TableColumn<DataCollected, String> lineColumn = new TableColumn<>("Line Count");
-        lineColumn.setMinWidth(200);
+        lineColumn.setMinWidth(100);
         lineColumn.setCellValueFactory(new PropertyValueFactory<>("lcount"));
 
         TableColumn<DataCollected, String> charColum = new TableColumn<>("Char Count");
-        charColum.setMinWidth(200);
+        charColum.setMinWidth(100);
         charColum.setCellValueFactory(new PropertyValueFactory<>("ccount"));
 
         TableColumn<DataCollected, String> sorceColumn = new TableColumn<>("Source Count");
-        sorceColumn.setMinWidth(200);
+        sorceColumn.setMinWidth(100);
         sorceColumn.setCellValueFactory(new PropertyValueFactory<>("slcount"));
 
         TableColumn<DataCollected, String> commentColumn = new TableColumn<>("Comment Count");
-        commentColumn.setMinWidth(200);
+        commentColumn.setMinWidth(100);
         commentColumn.setCellValueFactory(new PropertyValueFactory<>("cccount"));
 
         TableColumn<DataCollected, String> halnOneColumn = new TableColumn<>("n1 Count");
-        halnOneColumn.setMinWidth(200);
+        halnOneColumn.setMinWidth(100);
         halnOneColumn.setCellValueFactory(new PropertyValueFactory<>("operators"));
 
         TableColumn<DataCollected, String> halnTwoColumn = new TableColumn<>("n2 Count");
-        halnTwoColumn.setMinWidth(200);
+        halnTwoColumn.setMinWidth(100);
         halnTwoColumn.setCellValueFactory(new PropertyValueFactory<>("operands"));
 
         TableColumn<DataCollected, String> halN1Column = new TableColumn<>("N1 Count");
-        halN1Column.setMinWidth(200);
+        halN1Column.setMinWidth(100);
         halN1Column.setCellValueFactory(new PropertyValueFactory<>("operatorsT"));
 
         TableColumn<DataCollected, String> halN2Column = new TableColumn<>("N2 Count");
-        halN2Column.setMinWidth(200);
+        halN2Column.setMinWidth(100);
         halN2Column.setCellValueFactory(new PropertyValueFactory<>("operandsT"));
 
         /*
         TableColumn<DataCollected, String> cyclomaticComplexityColumn = new TableColumn<>("Cyclomatic Complexity");
-        commentColumn.setMinWidth(200);
-        commentColumn.setCellValueFactory(new PropertyValueFactory<>("cyclomaticcomplexity"));
+        commentColumn.setMinWidth(100);
+        commentColumn.setCellValueFactory(new PropertyValueFactory<>("cyclomaticcomplexity"));  //cyclomatic complexity
 
         TableColumn<DataCollected, String> maintainabilityIndexColumn = new TableColumn<>("Maintainability Index");
-        commentColumn.setMinWidth(200);
-        commentColumn.setCellValueFactory(new PropertyValueFactory<>("maintainabilityindex"));
+        commentColumn.setMinWidth(100);
+        commentColumn.setCellValueFactory(new PropertyValueFactory<>("maintainabilityindex"));  //maintainabilityindex
 
         TableColumn<DataCollected, String> depthOfInheritanceColumn = new TableColumn<>("Depth of Inheritance");
-        commentColumn.setMinWidth(200);
-        commentColumn.setCellValueFactory(new PropertyValueFactory<>("depthofinheritance"));
+        commentColumn.setMinWidth(100);
+        commentColumn.setCellValueFactory(new PropertyValueFactory<>("depthofinheritance"));    //depthofinheritance
         */
 
-        table.getColumns().addAll(nameColum, lineColumn,wordColum,charColum, commentColumn, sorceColumn, halnOneColumn, halnTwoColumn, halN1Column, halN2Column);
+        table.getColumns().addAll(nameColum, lineColumn,wordColum,charColum, commentColumn, sorceColumn, halnOneColumn,
+                halnTwoColumn, halN1Column, halN2Column);
         return table;
 
     }
