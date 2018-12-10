93246eeb7faccf9d6bdfa56730f67e5cd603db4a
fanciscoprado <franciscoprado@csus.edu>
2018-12-09 20:48:12 -0800

    added icon to new window

--- src/sample/CommitListDisplay.java
+++ src/sample/CommitListDisplay.java
@@ -5,7 +5,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -16,8 +18,11 @@ public class CommitListDisplay {
     ListView<String> listView;
     public void display(CommiterInfo commiterInfo){
         window = new Stage();
-        window.setTitle("ListView Demo");
+        window.setTitle("Commit Messages");
+        Image image = new Image("icon.png");
+        window.getIcons().add(image);
         Text name = new Text(commiterInfo.getName() + "'s total commit cout: " + commiterInfo.getCommits());
+        name.setFont(new Font(20));
         listView = new ListView<>();
 
         for(String temp : commiterInfo.getCommitMessageList()){
@@ -27,7 +32,7 @@ public class CommitListDisplay {
         listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(listView);
+        layout.getChildren().addAll(name,listView);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
