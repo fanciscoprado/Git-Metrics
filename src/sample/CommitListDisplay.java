f395165c3c2306bf809d2d824700b28ade80b392
fanciscoprado <franciscoprado@csus.edu>
2018-12-09 20:42:47 -0800

    removed button in commit message list

--- src/sample/CommitListDisplay.java
+++ src/sample/CommitListDisplay.java
@@ -6,19 +6,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class CommitListDisplay {
 
     Stage window;
     Scene scene;
-    Button button;
     ListView<String> listView;
     public void display(CommiterInfo commiterInfo){
         window = new Stage();
         window.setTitle("ListView Demo");
-        button = new Button("Submit");
-
+        Text name = new Text(commiterInfo.getName() + "'s total commit cout: " + commiterInfo.getCommits());
         listView = new ListView<>();
 
         for(String temp : commiterInfo.getCommitMessageList()){
@@ -28,7 +27,7 @@ public class CommitListDisplay {
         listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(listView, button);
+        layout.getChildren().addAll(listView);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
