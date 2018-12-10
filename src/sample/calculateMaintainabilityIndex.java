1c39b19e0a3264dd4e18a83fcc2567d9b8b19ae3
MichaelN <nguyen.mikey@yahoo.com>
2018-12-10 01:54:19 -0800

    Type casted double to int to round off numbers for cleaner output.

--- src/sample/calculateMaintainabilityIndex.java
+++ src/sample/calculateMaintainabilityIndex.java
@@ -6,7 +6,7 @@ import java.lang.Math;
 // Original formula: MI = 171 - 5.2 * ln(V) - 0.23 * (G) - 16.2 * ln(LOC)
 //                   mI = 171-5.2 * Math.log(V) - 0.23 * (CC) - 16.2 * Math.log(SLOC);
 
-//Formula(s) for calculating maintainability index found here: http://www.projectcodemeter.com/cost_estimation/help/GL_maintainability.htm
+//Formula for calculating maintainability index found here: http://www.projectcodemeter.com/cost_estimation/help/GL_maintainability.htm
 
 public class calculateMaintainabilityIndex {
     //Information that may or may not be implemented in output in the future.
@@ -16,8 +16,11 @@ public class calculateMaintainabilityIndex {
     //0-9 = low
     public double calculateMI(DataCollected dataCollected){
         double mI = 1.0;
-
         mI = 171-5.2 * Math.log(Float.valueOf(dataCollected.getVolume())) - 0.23 * (dataCollected.getCyclomaticcomplexity()) - 16.2 * Math.log(dataCollected.getSlcount());
-        return mI;
+        if(mI > 100.0){
+            mI = 100.0;
+        }
+        int mIRounded = (int) mI;
+        return mIRounded;
     }
 }
